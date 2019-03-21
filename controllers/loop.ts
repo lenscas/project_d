1b10711608d7e3ff3305173e50672eb81b136b05
lenscas <lenscas@gmail.com>
2019-03-21 12:05:12 +0100

    Add time check to event loop

--- controllers/loop.ts
+++ controllers/loop.ts
@@ -3,7 +3,7 @@ import easyErrors from "../wrappers/funcs";
 import { mpdServer } from "../config";
 import { MPC } from 'mpc-js';
 import Thing from '../wrappers/things';
-
+import connection from "../wrappers/db";
 class mpcPlayer implements Thing {
 
 	mpc: MPC
@@ -68,9 +68,50 @@ class mpcPlayer implements Thing {
 }
 
 const router: Router = Router();
-router.get('/', easyErrors(async (_, res) => {
-	let player = new mpcPlayer("mpcPlayer1", 0);
-	player.switchOn();
-	res.send("");
+const insertTime = async(con:connection, kind:string)=>{
+	let curTime = new Date().getTime()
+	await con.simpleInsert(
+		{
+			table:"config",
+			values: {
+				name: kind,
+				value: curTime.toString()
+			}
+		}
+	)
+	return new Date(curTime)
+}
+const getTime = async (con: connection,kind:string )=>{
+	const timeTBL = await con.find<{value:string}>({table:"config",name:"name",value:kind})
+	let time : Date;
+	if(timeTBL){
+		time = new Date(Number(timeTBL.value))
+	} else {
+		time = await insertTime(con,kind)
+	}
+	let now = new Date()
+	time.setFullYear(now.getFullYear(),now.getMonth(),now.getDate())
+	return time;
+}
+const isCloseEnough = (now: Date, mustBe:Date) : boolean => {
+	let isEarlier = mustBe <= now
+	if(isEarlier){
+		console.log(now)
+		now.setMinutes(now.getMinutes() - 5);
+		
+		console.log(mustBe)
+		return mustBe >= now;
+	}
+	return false
+}
+router.get('/', easyErrors(async (_, res,con) => {
+	let start = await getTime(con,"start")
+	const now = new Date()
+	if(isCloseEnough(now,start) ) {
+		let player = new mpcPlayer("mpcPlayer1", 0);
+		player.switchOn();
+		res.send("");
+	}
+	
 }));
 export const LoopController: Router = router;
