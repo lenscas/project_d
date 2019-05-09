9610ab8349eed29ac8b56e2dea68a05c21402ea5
lenscas <lenscas@gmail.com>
2019-05-09 11:48:59 +0200

    Add method to set time to sleep

--- controllers/settings.ts
+++ controllers/settings.ts
@@ -9,10 +9,30 @@ const router: Router = Router();
 // The / here corresponds to the route that the WelcomeController
 // is mounted on in the server.ts file.
 // In this case it's /settings
-router.get('/', (req: Request, res: Response) => {
-    // Reply with a hello world when no name param is provided
-	res.send('Hello, World!');
-});
+router.post('/time', easyErrors( async (req,res, con) => {
+	if(req.body.new_time) {
+		const found = await con.find({
+			table : "config",
+			name : "name",
+			value : "start"
+		})
+		if(!found){
+			await con.simpleInsert({
+				table : "config",
+				values : {
+					name : "start",
+					value : req.body.new_time
+				}
+			})
+		} else {
+			await con.query({
+				sql : 'UPDATE config SET value=? WHERE name="start" LIMIT 1',
+				values : [req.body.new_time]
+			})
+		}
+		res.send({success:true})
+	}
+}));
 
 // Export the express.Router() instance to be used by server.ts
 export const SettingsController: Router = router;
