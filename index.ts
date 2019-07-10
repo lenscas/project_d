5f7ad71e4f52151c4559557b55b84f1ebac985fd
lenscas <lenscas@gmail.com>
2019-07-10 16:10:40 +0200

    add interval

--- index.ts
+++ index.ts
@@ -11,9 +11,9 @@ import { WelcomeController } from "./controllers/welcome"
 import { SettingsController } from "./controllers/settings"
 import { LoopController } from "./controllers/loop";
 import { AudioController } from "./controllers/audio";
-
+import mpcPlayer from "./things/mpcPLayer";
+import Light from "./things/light";
 /*
-import light from "./things/light";
 
 const lamp = new light("a", 1)
 lamp.setAction(10)
@@ -69,4 +69,52 @@ app.listen(server.port, () => {
     // Success callback
     console.log(`Listening at http://localhost:${server.port}/`);
 });
+const insertTime = async(con:dbWrapper, kind:string)=>{
+	let curTime = new Date().getTime()
+	await con.simpleInsert(
+		{
+			table:"config",
+			values: {
+				name: kind,
+				value: curTime.toString()
+			}
+		}
+	)
+	return new Date(curTime)
+}
+const getTime = async (con: dbWrapper,kind:string )=>{
+	const timeTBL = await con.find<{value:string}>({table:"config",name:"name",value:kind})
+	let time : Date;
+	if(timeTBL){
+		time = new Date(Number(timeTBL.value))
+	} else {
+		time = await insertTime(con,kind)
+	}
+	let now = new Date()
+	time.setFullYear(now.getFullYear(),now.getMonth(),now.getDate())
+	return time;
+}
+const isCloseEnough = (now: Date, mustBe:Date) : boolean => {
+	let isEarlier = mustBe <= now
+	if(isEarlier){
+		console.log(now)
+		now.setMinutes(now.getMinutes() - 5);
+		
+		console.log(mustBe)
+		return mustBe >= now;
+	}
+	return false
+}
+setInterval(async ()=>{
+	const con = new dbWrapper(connection);
+	let start = await getTime(con,"start")
+	const now = new Date()
+	let light = new Light("light",0,0);
+	light.setAction(1);
+
+	if(isCloseEnough(now,start) ) {
+		let player = new mpcPlayer("mpcPlayer1", 0);
+		player.switchOn();
+	}
+})
 
